06f87f1e267c278972869c8e31df08e8e99a4884
Veetaha <veetaha2@gmail.com>
2020-08-14 02:30:49 +0300

    Add pre-commit hook to xtask

--- xtask/src/lib.rs
+++ xtask/src/lib.rs
@@ -1,3 +1,9 @@
-pub fn foo() {
-    todo!("Write this crate");
+pub mod pre_commit;
+
+use std::path::{Path, PathBuf};
+use once_cell::sync::OnceCell;
+
+fn project_root_dir() -> &'static Path {
+    static DIR: OnceCell<PathBuf> = OnceCell::new();
+    DIR.get_or_init(|| devx_pre_commit::locate_project_root().unwrap())
 }
---
+++ xtask/src/main.rs
@@ -0,0 +1,36 @@
+//! This a dev cli, see `HELP` strings for details.
+
+use anyhow::Result;
+use std::env;
+use xtask::pre_commit;
+
+const HELP: &str = "\
+xtask
+This binary defines auxiliary ad-hoc scripts that serve as a replacement for
+nasty enormous bash scripts. This is a dev cli, and it is intended to be used
+only by the developers and not for production distribution.
+See https://github.com/matklad/cargo-xtask/ for more info.
+
+USAGE:
+    xtask <SUBCOMMAND>
+
+SUBCOMMANDS:
+    install-pre-commit-hook    Install git pre-commit hook that formats code with `cargo fmt`";
+
+fn main() -> Result<()> {
+    if let Some(true) = env::args().next().map(|it| it.contains("pre-commit")) {
+        return pre_commit::run_hook();
+    }
+
+    let mut args = pico_args::Arguments::from_env();
+
+    match args.subcommand()?.unwrap_or_default().as_str() {
+        "install-pre-commit-hook" => {
+            args.finish()?;
+            pre_commit::install_hook()?;
+            eprintln!("Git pre-commit hook is successfully installed. Have a nice day :D");
+        }
+        _ => eprintln!("{}", HELP),
+    }
+    Ok(())
+}
