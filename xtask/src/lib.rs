4946cc22701159a91eeda615865c97129bdccb9f
Veetaha <veetaha2@gmail.com>
2020-08-14 02:47:19 +0300

    Run cargo fmt

--- xtask/src/lib.rs
+++ xtask/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod pre_commit;
 
-use std::path::{Path, PathBuf};
 use once_cell::sync::OnceCell;
+use std::path::{Path, PathBuf};
 
 fn project_root_dir() -> &'static Path {
     static DIR: OnceCell<PathBuf> = OnceCell::new();
